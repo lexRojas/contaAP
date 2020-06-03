b0106c67bc6e4a85269fd720d5d7804c81bf3b8b
lexrojas <legalcr.rojas@gmail.com>
2020-06-03 16:49:43 -0600

    acceso bat

---
+++ conta.bat
@@ -0,0 +1,7 @@
+cd C:\Users\Usuario\OneDrive\CttpApps\ssh\stockCTPP
+git fetch origin 
+git merge
+copy *.jar C:\Users\Usuario\OneDrive\CttpApps\StockAP /Y
+cd C:\Users\Usuario\OneDrive\CttpApps\StockAP 
+
+java -jar StockAp.jar
