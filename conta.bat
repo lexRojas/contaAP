16f5211baab5df3973705f7ffd69113b82ae1609
lexrojas <legalcr.rojas@gmail.com>
2020-06-03 16:56:36 -0600

    pdkkdkdk d dmdkd

--- conta.bat
+++ conta.bat
@@ -1,7 +1,4 @@
-cd C:\Users\Usuario\OneDrive\CttpApps\ssh\stockCTPP
 git fetch origin 
 git merge
-copy *.jar C:\Users\Usuario\OneDrive\CttpApps\StockAP /Y
-cd C:\Users\Usuario\OneDrive\CttpApps\StockAP 
 
-java -jar StockAp.jar
+java -jar ContaAPP.jar
